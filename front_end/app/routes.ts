d2f7301205299433c44a5d498d9dc4f3b518e13b
mathew15064 <ttnguyen1564@gmail.com>
2025-04-14 19:23:26 +0700

    add init

--- front_end/app/routes.ts
+++ front_end/app/routes.ts
@@ -1,3 +1,24 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+    type RouteConfig,
+    index,
+    layout,
+    prefix,
+    route,
+} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    route("", "src/pages/user/User.tsx",),
+    route("login", "src/pages/login/Login.tsx"),
+    // ...prefix("concerts", [
+        // index("src/pages/user/User.tsx"),
+    //     layout("src/layout/Main.tsx", [
+    //         index("src/pages/user/User.tsx",),
+    //         route("/login", "src/pages/login/Login.tsx"),
+    //       ]),
+    //   ]),
+    // layout("src/layout/Main.tsx", [
+    //     route("hello", "src/pages/user/User.tsx",),
+    // ]),
+    
+
+] satisfies RouteConfig;
