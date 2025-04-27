73b1a2fee29d1732e2196edf63599734f051cfc8
MSI\Phuong Anh <ltpa1203@gmail.com>
2025-04-27 23:17:53 +0700

    commit code 20250427

--- front_end/app/pages/login/LoginPage.tsx
+++ front_end/app/pages/login/LoginPage.tsx
@@ -1,19 +1,122 @@
-import type { Route } from "../../+types/root";
+import React, { useState } from "react";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import { Checkbox } from "primereact/checkbox";
+import { IconField } from "primereact/iconfield";
+import { InputIcon } from "primereact/inputicon";
+import "primereact/resources/themes/lara-light-blue/theme.css"; // Đảm bảo import theme
+import "primereact/resources/primereact.min.css";
+import "./LoginPage.scss";
+import axios from "axios";
+import Cookies from "js-cookie";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "Login" },
-    { name: "description", content: "Login to Hotel Admin" },
-  ];
+interface LoginForm {
+  email: string;
+  password: string;
+  remember: boolean;
 }
 
-export default function Login() {
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [remember, setRemember] = useState<boolean>(false);
+
+  const handleSubmit = async (e: any): Promise<void> => {
+    e.preventDefault();
+    const loginData: LoginForm = { email, password, remember };
+    try {
+      const response = await axios.post(
+        import.meta.env.VITE_API_BACKEND + "/auth/login",
+        loginData
+      );
+      if (response.data.code == 200) {
+        Cookies.set("token", response.data.result.token);
+        Cookies.set("refreshtoken", response.data.result.refreshToken);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
-    <div>
-      <h1 className="text-black">This is Login Page</h1>
-      <form>
-        
-      </form>
+    <div className="login-wrapper">
+    <div className="hidden-logo">
+      <i className="fas fa-hotel"></i>
+    </div>
+    <div className="login-card">
+      <div className="login-header">
+        <h1 className="login-title">
+          <img src="app/asset/images/minilogo.png" alt="Hotel Logo" className="header-logo" />
+          Portal Hotel Management
+        </h1>
+        <p className="subtitle">Sign in to stay connected.</p>
+      </div>  
+
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <IconField iconPosition="left">
+              <InputIcon className="pi pi-envelope" />
+              <InputText
+                id="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full"
+              />
+            </IconField>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <IconField iconPosition="left">
+              <InputIcon className="pi pi-lock" />
+              <InputText
+                id="password"
+                placeholder="Password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full"
+              />
+            </IconField>
+          </div>
+
+          <div className="remember-forgot-container">
+            <div className="remember-me">
+              <Checkbox
+                inputId="remember"
+                checked={remember}
+                onChange={(e) => setRemember(e.checked ?? false)}
+              />
+              <label htmlFor="remember">Remember me?</label>
+            </div>
+            <a href="#" className="forgot-password">
+              Forgot Password
+            </a>
+          </div>
+
+          <Button label="Sign in" className="sign-in-button" type="submit" />
+
+          <div className="or-divider">
+            <span>or sign in with other accounts?</span>
+          </div>
+
+          <div className="social-login">
+    <Button
+      icon="pi pi-google"
+      className="google-login-button"
+      aria-label="Google"
+     
+    />
+  </div>
+
+          <div className="sign-up-prompt">
+            Don't have an account? <a href="/signup">Click here to sign up.</a>
+          </div>
+        </form>
+      </div>
     </div>
   );
-}
+};
+
+export default LoginPage;
--- front_end/vite.config.ts
+++ front_end/vite.config.ts
@@ -2,9 +2,10 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import path from 'path';
+import envCompatible from 'vite-plugin-env-compatible';
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [tailwindcss(), reactRouter(), envCompatible()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './app')
